ab1813437b047cec469fffa184e01cef55fb15c2
MoriartyWalter <rohanmoriarty@gmail.com>
2025-08-04 12:11:47 +0530

    first commit

---
+++ src/main/java/com/example/test/TestMyMeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMyMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMyMeshApplication.class, args);
+	}
+
+}
